dc706d3765b36636ddf0c9fb25ff48ec6bb7ceaa
root <root@MERIT-030330.merit.edu>
2018-03-21 11:46:31 -0400

    Added an icon and background

--- build/kernel/CMakeFiles/storagemgr.skprx.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/storagemgr.skprx"
-  "storagemgr.velf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/storagemgr.skprx.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
